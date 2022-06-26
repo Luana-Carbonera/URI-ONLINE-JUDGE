05f48e5cda06a01506c6fb6eb35f7c160c629dc5
Luana Carbonera <luana.lcarbonera@gmail.com>
2022-06-26 18:24:19 -0300

    Problem Solved

---
+++ PYTHON/Problem 1052 - MÊS.py
@@ -0,0 +1,38 @@
+'''
+Leia um número inteiro entre 1 e 12, inclusive. Correspondente a este número,
+deve-se imprimir o mês do ano, em inglês, com a primeira letra maiúscula.
+
+Entrada
+A entrada contém apenas um número inteiro.
+
+Resultado
+Imprima o nome do mês de acordo com o número inserido, com a primeira letra em maiúscula.
+'''
+x = int(input())
+
+if x == 1:
+    print("January")
+elif x == 2:
+    print("February")
+elif x == 3:
+    print("March")
+elif x == 4:
+    print("April")
+elif x == 5:
+    print("May")
+elif x == 6:
+    print("June")
+elif x == 7:
+    print("July")
+elif x == 8:
+    print("August")
+elif x == 9:
+    print("September")
+elif x == 10:
+    print("October")
+elif x == 11:
+    print("November")
+elif x == 12:
+    print("December")
+
+
